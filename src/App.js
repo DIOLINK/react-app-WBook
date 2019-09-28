6ec28e575df6f91bbb388f840b4ecec9e5c15bf5
ROGER COVERZOLA BOGADO <diolink.4103@gmail.com>
2019-09-27 22:01:54 -0300

    index.css and App.js rebuild start :´(

--- src/App.js
+++ src/App.js
@@ -1,42 +1,12 @@
 import React, {Component} from 'react';
 
-
-// class App extends Component {
-//   constructor(){
-//     super();    
-//     this.state = {items: [],
-//     filter: null
-//     }
-//   } 
-//   componentWillMount(){
-//     fetch('http://swapi.co/api/people/?format=json')
-//     .then( response => response.json() )
-//     .then( ({results: items}) => this.setState({items}))
-//     .catch(e => {
-//       console.log(e)
-//     })
-//     console.log('paso por aca');
-    
-//   }
-//   filter(e){
-//     this.setState({filter: e.target.value})
-//   }
-//     render(){ 
-//       let items = this.state.items;
-//       console.log(items);
-//       if(this.state.filter){
-//         items = items.filter(item => item.name.toLowerCase().includes(this.state.filter.toLowerCase()))
-//       }
-//       return(
-//             <div>
-//               <input type="text" onChange={this.filter.bind(this)} />
-//               {items.map(item => <Person person={item} />)}
-//             </div>
-//       )
-//     }
-// }
-// const Person = (props) => <h4>{props.person.name}</h4>
-
+class App extends Component {
+    render(){
+        return(
+            
+        )
+    }
+}
 
 
 export default App;
--- src/page/searchbar/layout.js
+++ src/page/searchbar/layout.js
@@ -5,15 +5,15 @@ import Search from '../images/search.svg';
 class SearchBar extends Component {
     render(){
         return(
-            <div id="b0">
-              <div id="b1" >
+            <div id="saerch">
+              <div id="search1" >
                     <select ref="filtro" id="filtro" >
                         <option value="none">Seleccionar filtro</option>
                         <option value="nombre">Nombre</option>
                         <option value="autor">Autor</option>
                     </select>
               </div>
-               <div id="b2" >
+               <div id="search2" >
                     <input type="text" value="Buscar.." ref="buscar" id="txtbuscar" />
                         <div id="search">
                             <img src={Search} width="24px" height="24px"  />
