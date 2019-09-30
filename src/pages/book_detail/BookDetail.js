a965087102a5b2505f05ad65537021614542255f
ROGER COVERZOLA BOGADO <diolink.4103@gmail.com>
2019-09-30 20:42:13 -0300

    BookDetail Init loyout

---
+++ src/pages/book_detail/BookDetail.js
@@ -0,0 +1,15 @@
+import React, {Componet} from 'react';
+
+import Head from './header/layout';
+
+class BookDetail extends Componet {
+
+    render(){
+        return(
+            <Head />
+            
+        )
+    }
+}
+
+export default BookDetail;
