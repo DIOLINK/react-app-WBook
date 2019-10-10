3523971eebe834926fa9f60cf715cfacfe83d25e
Roger Coverzola Bogado <diolink.4103@gmail.com>
2019-10-09 22:41:12 -0300

    Filter

--- src/pages/home/searchbar/layout.js
+++ src/pages/home/searchbar/layout.js
@@ -7,16 +7,16 @@ import "../searchbar/Searchbar.css";
 import Books from "../book/loyout";
 
 class SearchBar extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state = {text:'', value:''};
+    this.state = { text: "", value: "", filterbooks: [] };
     this.searchText = this.searchText.bind(this);
+    this.searchSelect = this.searchSelect.bind(this);
     this.submit = this.submit.bind(this);
-
   }
 
   searchText(e) {
-    this.setState({ text: e.target.text });
+    this.setState({ text: e.target.value });
   }
 
   searchSelect(e) {
@@ -24,39 +24,55 @@ class SearchBar extends Component {
   }
 
   submit(e) {
-    
-    return console.log(this.state.text + " " + this.state.value);
+    if (this.state.value === "title") {
+      let filterbooks = this.props.books.filter(book => book.title === this.state.text);
+      this.setState({ filterbooks: filterbooks });
+    } else if (this.state.value === "author") {
+      let filterbooks = this.props.books.filter();
+      this.setState({ filterbooks: filterbooks });
+    }
   }
 
   render() {
-    const IMG_BOOK_ICON = "24px";  
-    
+    const IMG_BOOK_ICON = "24px";
+
     return (
       <div id="saerch">
-          <div id="saerch1">
-            <select ref="filtro" id="filtro" onChange={this.searchSelect} >
+        <div id="contSaerch">
+          <div id="filter">
+            <select
+              id="filtro"
+              onChange={this.searchSelect}
+              value={this.state.value}
+            >
               <option value="none">Seleccionar filtro</option>
               <option value="title">Nombre</option>
               <option value="author">Autor</option>
             </select>
           </div>
           <div id="search2">
-            <input id="txtbuscar" onChange={this.searchText} value={this.state.text} />
+            <input
+              id="txtbuscar"
+              onChange={this.searchText}
+              value={this.state.text}
+            />
             <div id="imgSearch">
-                <img
-                  src={Search}
-                  width={IMG_BOOK_ICON}
-                  height={IMG_BOOK_ICON}
-                  onClick={this.submit}
-                />
+              <img
+                src={Search}
+                width={IMG_BOOK_ICON}
+                height={IMG_BOOK_ICON}
+                onClick={this.submit}
+              />
             </div>
           </div>
+        </div>
         <div id="book">
-          <Books books={this.props.books} />
+          <Books books={this.state.filterbooks} />
         </div>
       </div>
     );
   }
 }
+
 
 export default SearchBar;
